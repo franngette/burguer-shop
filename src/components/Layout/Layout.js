185ea37bbc3f84eabeaea78f15e663f4136a5f0c
Francis Gette <franngette@gmail.com>
2021-02-22 23:56:27 -0300

    added BuildControls

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from "react";
 
+import Auxiliar from "../../hoc/Auxiliar";
 import classes from "./Layout.module.css";
 
 const layout = (props) => (
-  <>
+  <Auxiliar>
     <div>Toolbar, SideDrawer, Backdrop</div>
     <main className={classes.Content}>{props.children}</main>
-  </>
+  </Auxiliar>
 );
 
 export default layout;
